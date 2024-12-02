56dd3ebfe15efb508c7ab6ae6f2d7d1f3370e186
saulorodrig <saulofelippe791@gmail.com>
2024-12-02 17:26:06 -0300

    Update socket.js

--- api/server.js
+++ api/server.js
@@ -1,37 +1,42 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+import { Server } from 'socket.io';
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+        res.status(200).send('Servidor de WebSocket está ativo');
+    } else {
+        res.status(405).send('Método não permitido');
+    }
+}
 
-app.use(express.static(__dirname + '/public'));
+// Crie o WebSocket Server na Vercel
+export function startSocketServer() {
+    const io = new Server();
 
-io.on('connection', socket => {
-    socket.on('host-ready', () => {
-        socket.broadcast.emit('host-ready');
+    io.on('connection', socket => {
+        socket.on('host-ready', () => {
+            socket.broadcast.emit('host-ready');
+        });
+
+        socket.on('offer', (id, description) => {
+            io.to(id).emit('offer', socket.id, description);
+        });
+
+        socket.on('answer', (id, description) => {
+            io.to(id).emit('answer', socket.id, description);
+        });
+
+        socket.on('candidate', (id, candidate) => {
+            io.to(id).emit('candidate', socket.id, candidate);
+        });
+
+        socket.on('watcher', () => {
+            socket.broadcast.emit('watcher', socket.id);
+        });
+
+        socket.on('disconnect', () => {
+            socket.broadcast.emit('disconnectPeer', socket.id);
+        });
     });
 
-    socket.on('offer', (id, description) => {
-        io.to(id).emit('offer', socket.id, description);
-    });
-
-    socket.on('answer', (id, description) => {
-        io.to(id).emit('answer', socket.id, description);
-    });
-
-    socket.on('candidate', (id, candidate) => {
-        io.to(id).emit('candidate', socket.id, candidate);
-    });
-
-    socket.on('watcher', () => {
-        socket.broadcast.emit('watcher', socket.id);
-    });
-
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('disconnectPeer', socket.id);
-    });
-});
-
-server.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    return io;
+}
